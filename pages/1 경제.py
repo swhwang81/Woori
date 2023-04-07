85fa04dfaf2679e6a3661fcb703476cf721c9533
swhwang <83943446+swhwang81@users.noreply.github.com>
2023-04-07 17:02:02 +0900

    Add files via upload

--- pages/1 경제.py
+++ pages/1 경제.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+st.write("This is Testing...")
+
 fig, ax = plt.subplots()
 
 fruits = ['apple', 'blueberry', 'cherry', 'orange']
